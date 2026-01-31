42392a62294658cea97c790ac7cdb4b8b9602903
soul_kk <2986744287@qq.com>
2026-01-31 16:51:52 +0800

    update supabse config

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://xabmfdhigikjrjinipiw.supabase.co';
-const supabaseKey = 'sb_publishable_HEWQi1iWz5U01xla_tcvhA_k8dF1hA1';
+export const supabaseUrl = 'https://yyvtrrpgcpkujvzxjjdf.supabase.co';
+const supabaseKey = 'sb_publishable_foi9Gxdvm1Dmd9ZzT8p6Pw_sj-IXOvG';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
